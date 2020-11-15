31f7517189b32edf8245e429e5c807cfd0b48f96
Kamran Haq <kamranhaq@protonmail.com>
2020-11-14 21:01:53 -0800

    Added Junit 4 dependency and added basic test classes. Signed-off-by: Kamran Haq <kamranhaq@protonmail.com>

---
+++ WTRSimlib/src/main/java/simulation/testing/AllTests.java
@@ -0,0 +1,15 @@
+package simulation.testing;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+        BasicTestClass.class,
+        //More tests go here
+        })
+
+public class AllTests {
+
+}
